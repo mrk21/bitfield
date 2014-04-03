a158a9773e8fcdc9cf70c292060c86ce8a9d1d6a
Yuichi Murata <mrk21info+github@gmail.com>
2014-04-03 20:00:04 +0900

    Added files.

---
+++ bitfield/bitfield.hpp
@@ -0,0 +1,107 @@
+#ifndef __INCLUDED_BITFIELD_HPP__
+#define __INCLUDED_BITFIELD_HPP__
+
+#include <iostream>
+#include <array>
+
+namespace bitfield {
+    class bit_type {
+        uint8_t v;
+        
+    public:
+        constexpr bit_type(uint8_t v) : v(v) {}
+        
+        constexpr bit_type floor() const {
+            return bit_type(this->v >> 3 << 3);
+        }
+        
+        constexpr bit_type ceil() const {
+            return bit_type((this->v + 7) >> 3 << 3);
+        }
+        
+        constexpr uint32_t bit() const {
+            return this->v;
+        }
+        
+        constexpr uint32_t byte() const {
+            return this->v >> 3;
+        }
+        
+        constexpr uint32_t max_value() const {
+            return this->v >= 32 ? 0xffffffff : (0x01 << this->v) - 0x01;
+        }
+        
+        constexpr bit_type diff(bit_type target) const {
+            return bit_type(this->v < target.v ? target.v - this->v
+                                               : this->v - target.v);
+        };
+        
+        template<typename T>
+        constexpr uint8_t * addr(const T * addr) const {
+            return (uint8_t *)addr + this->byte();
+        }
+    };
+    
+    template<uint32_t Size, uint32_t Offset = 0>
+    class bitfield {
+    public:
+        static constexpr uint32_t SIZE = Size;
+        static constexpr uint32_t OFFSET = Offset;
+        static constexpr uint32_t NEXT_OFFSET = SIZE + OFFSET;
+        
+        static constexpr uint32_t BYTE = bit_type(SIZE).ceil().byte();
+        static constexpr uint32_t PADDING = bit_type(NEXT_OFFSET).ceil().diff(NEXT_OFFSET).bit();
+        static constexpr uint32_t MASK = bit_type(SIZE).max_value() << PADDING;
+        
+        template<uint32_t NextSize>
+        using next_bitfield = bitfield<NextSize, NEXT_OFFSET>;
+        using container_type = std::array<uint8_t, bit_type(NEXT_OFFSET).ceil().byte()>;
+        
+        // for Little-Endian
+        uint32_t get() const {
+            uint8_t * addr = bit_type(OFFSET).addr(this);
+            uint32_t value = 0x00;
+            
+            for (uint32_t i=0; i < BYTE; ++i) {
+                value |= addr[i] << ((BYTE-i-1)<<3);
+            }
+            
+            value &= MASK;
+            value >>= PADDING;
+            return value;
+        }
+        
+        // for Little-Endian
+        void set(uint32_t value) {
+            static constexpr uint32_t clear_mask = ~MASK;
+            
+            value <<= PADDING;
+            value &= MASK;
+            
+            uint8_t * addr = bit_type(OFFSET).addr(this);
+            uint8_t * value_addr = (uint8_t *)&value;
+            uint8_t * clear_mask_addr = (uint8_t *)&clear_mask;
+            
+            for (uint32_t i=0; i < BYTE; ++i) {
+                uint32_t j = BYTE-i-1;
+                addr[i] &= clear_mask_addr[j];
+                addr[i] |= value_addr[j];
+            }
+        }
+        
+        operator uint32_t() const {
+            return this->get();
+        }
+        
+        bitfield & operator =(uint32_t value) {
+            this->set(value);
+            return *this;
+        }
+        
+        friend std::ostream & operator <<(std::ostream & out, const bitfield & v) {
+            return out << v.get();
+        }
+    };
+}
+
+#endif // __INCLUDED_BITFIELD_HPP__
---
+++ test/spec.cpp
@@ -0,0 +1,134 @@
+#include <bandit/bandit.h>
+#include <bitfield/bitfield.hpp>
+
+namespace bitfield {
+    union bitfield_test {
+        using v1_type =               bitfield< 1>;
+        using v2_type = v1_type::next_bitfield< 9>;
+        using v3_type = v2_type::next_bitfield<14>;
+        using v4_type = v3_type::next_bitfield< 8>;
+        using container_type = v4_type::container_type;
+        
+        container_type container;
+        v1_type v1;
+        v2_type v2;
+        v3_type v3;
+        v4_type v4;
+    };
+    
+go_bandit([&]{
+    using namespace bandit;
+    
+    describe("bit_type", [&]{
+        describe("#floor()", [&]{
+            describe("when value is 0" , [&]{ it("should be 0" , [&]{ AssertThat(bit_type( 0).floor().bit(), Equals( 0)); }); });
+            describe("when value is 7" , [&]{ it("should be 0" , [&]{ AssertThat(bit_type( 7).floor().bit(), Equals( 0)); }); });
+            describe("when value is 8" , [&]{ it("should be 8" , [&]{ AssertThat(bit_type( 8).floor().bit(), Equals( 8)); }); });
+            describe("when value is 9" , [&]{ it("should be 8" , [&]{ AssertThat(bit_type( 9).floor().bit(), Equals( 8)); }); });
+            describe("when value is 15", [&]{ it("should be 8" , [&]{ AssertThat(bit_type(15).floor().bit(), Equals( 8)); }); });
+            describe("when value is 16", [&]{ it("should be 16", [&]{ AssertThat(bit_type(16).floor().bit(), Equals(16)); }); });
+            describe("when value is 17", [&]{ it("should be 16", [&]{ AssertThat(bit_type(17).floor().bit(), Equals(16)); }); });
+        });
+        
+        describe("#ceil()", [&]{
+            describe("when value is 0" , [&]{ it("should be 0" , [&]{ AssertThat(bit_type( 0).ceil().bit(), Equals( 0)); }); });
+            describe("when value is 7" , [&]{ it("should be 8" , [&]{ AssertThat(bit_type( 7).ceil().bit(), Equals( 8)); }); });
+            describe("when value is 8" , [&]{ it("should be 8" , [&]{ AssertThat(bit_type( 8).ceil().bit(), Equals( 8)); }); });
+            describe("when value is 9" , [&]{ it("should be 16", [&]{ AssertThat(bit_type( 9).ceil().bit(), Equals(16)); }); });
+            describe("when value is 15", [&]{ it("should be 16", [&]{ AssertThat(bit_type(15).ceil().bit(), Equals(16)); }); });
+            describe("when value is 16", [&]{ it("should be 16", [&]{ AssertThat(bit_type(16).ceil().bit(), Equals(16)); }); });
+            describe("when value is 17", [&]{ it("should be 24", [&]{ AssertThat(bit_type(17).ceil().bit(), Equals(24)); }); });
+        });
+        
+        describe("#max_value()", [&]{
+            describe("when value is 0" , [&]{ it("should be 0"         , [&]{ AssertThat(bit_type( 0).max_value(), Equals(         0)); }); });
+            describe("when value is 10", [&]{ it("should be 1023"      , [&]{ AssertThat(bit_type(10).max_value(), Equals(      1023)); }); });
+            describe("when value is 32", [&]{ it("should be 4294967295", [&]{ AssertThat(bit_type(32).max_value(), Equals(4294967295)); }); });
+            describe("when value is 33", [&]{ it("should be 4294967295", [&]{ AssertThat(bit_type(33).max_value(), Equals(4294967295)); }); });
+        });
+        
+        describe("#diff(bit_type target)", [&]{
+            describe("when value is 7 and target is 5", [&]{ it("should be 2", [&]{ AssertThat(bit_type(7).diff(5).bit(), Equals(2)); }); });
+            describe("when value is 5 and target is 7", [&]{ it("should be 2", [&]{ AssertThat(bit_type(5).diff(7).bit(), Equals(2)); }); });
+        });
+        
+        describe("#addr(const T * addr)", [&]{
+            it("should be `addr + #byte()`", [&]{
+                bit_type bit(5);
+                uint32_t * addr = 0x00;
+                AssertThat(bit.addr(addr), Equals((uint8_t *)addr + bit.byte()));
+            });
+        });
+    });
+    
+    describe("bitfield", [&]{
+        bitfield_test * subject;
+        
+        before_each([&]{
+            // |1byte        |2byte      |3byte     |4byte      |
+            // | 1|000 1100  11|11 1001  0110 1110  |1101 1001  |
+            // |v1|v2          |v3                  |v4         |
+            // | 1|51          |14702               |217        |
+            subject = new bitfield_test{{{
+                0b10001100,
+                0b11111001,
+                0b01101110,
+                0b11011001,
+            }}};
+        });
+        
+        describe("#get()", [&]{
+            describe("v1", [&]{ it("should be 1"    , [&]{ AssertThat(subject->v1.get(), Equals(    1)); }); });
+            describe("v2", [&]{ it("should be 51"   , [&]{ AssertThat(subject->v2.get(), Equals(   51)); }); });
+            describe("v3", [&]{ it("should be 14702", [&]{ AssertThat(subject->v3.get(), Equals(14702)); }); });
+            describe("v4", [&]{ it("should be 217"  , [&]{ AssertThat(subject->v4.get(), Equals(  217)); }); });
+        });
+        
+        describe("#set(uint32_t value)", [&]{
+            describe("when v1 is 0", [&]{
+                before_each([&]{
+                    subject->v1.set(0);
+                });
+                describe("v1", [&]{ it("should be 0"    , [&]{ AssertThat(subject->v1.get(), Equals(    0)); }); });
+                describe("v2", [&]{ it("should be 51"   , [&]{ AssertThat(subject->v2.get(), Equals(   51)); }); });
+                describe("v3", [&]{ it("should be 14702", [&]{ AssertThat(subject->v3.get(), Equals(14702)); }); });
+                describe("v4", [&]{ it("should be 217"  , [&]{ AssertThat(subject->v4.get(), Equals(  217)); }); });
+            });
+            
+            describe("when v2 is 120", [&]{
+                before_each([&]{
+                    subject->v2.set(120);
+                });
+                describe("v1", [&]{ it("should be 1"    , [&]{ AssertThat(subject->v1.get(), Equals(    1)); }); });
+                describe("v2", [&]{ it("should be 120"  , [&]{ AssertThat(subject->v2.get(), Equals(  120)); }); });
+                describe("v3", [&]{ it("should be 14702", [&]{ AssertThat(subject->v3.get(), Equals(14702)); }); });
+                describe("v4", [&]{ it("should be 217"  , [&]{ AssertThat(subject->v4.get(), Equals(  217)); }); });
+            });
+            
+            describe("when v3 is 48", [&]{
+                before_each([&]{
+                    subject->v3.set(48);
+                });
+                describe("v1", [&]{ it("should be 1"  , [&]{ AssertThat(subject->v1.get(), Equals(  1)); }); });
+                describe("v2", [&]{ it("should be 51" , [&]{ AssertThat(subject->v2.get(), Equals( 51)); }); });
+                describe("v3", [&]{ it("should be 48" , [&]{ AssertThat(subject->v3.get(), Equals( 48)); }); });
+                describe("v4", [&]{ it("should be 217", [&]{ AssertThat(subject->v4.get(), Equals(217)); }); });
+            });
+            
+            describe("when v4 is 123", [&]{
+                before_each([&]{
+                    subject->v4.set(123);
+                });
+                describe("v1", [&]{ it("should be 1"    , [&]{ AssertThat(subject->v1.get(), Equals(    1)); }); });
+                describe("v2", [&]{ it("should be 51"   , [&]{ AssertThat(subject->v2.get(), Equals(   51)); }); });
+                describe("v3", [&]{ it("should be 14702", [&]{ AssertThat(subject->v3.get(), Equals(14702)); }); });
+                describe("v4", [&]{ it("should be 123"  , [&]{ AssertThat(subject->v4.get(), Equals(  123)); }); });
+            });
+        });
+    });
+});
+}
+
+int main(int argc, char * argv[]) {
+    return bandit::run(argc, argv);
+}
