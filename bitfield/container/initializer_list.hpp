21d7d8e7c6f9a008fbbb21cf212367f1a7c3d7b5
Yuichi Murata <mrk21info+github@gmail.com>
2014-04-14 18:53:25 +0900

    Added the container::initializer_list.

---
+++ bitfield/container/initializer_list.hpp
@@ -0,0 +1,10 @@
+#ifndef __INCLUDED_BITFIELD_CONTAINER_INITIALIZER_LIST_HPP__
+#define __INCLUDED_BITFIELD_CONTAINER_INITIALIZER_LIST_HPP__
+
+#include <initializer_list>
+
+namespace bitfield { namespace container {
+    using initializer_list = std::initializer_list<uint8_t>;
+}};
+
+#endif // __INCLUDED_BITFIELD_CONTAINER_INITIALIZER_LIST_HPP__
